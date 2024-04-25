b825382f7b23c0e3773c5e4b3c124d34824f6c01
muhammadashiqurrahman <getintouch.ashiq@gmail.com>
2024-04-25 13:14:51 +0600

    react with shadcn starter pack

---
+++ README.md
@@ -0,0 +1,3 @@
+## using reactjs and shadcn
+
+## before start the code remove/delete .git file
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      Main Conent
+    </div>
+  );
+};
+
+export default Home;
